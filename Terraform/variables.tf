e353711d534282ad25d59bec661bc69f184133cf
Ankit007Kumar <146662315+Ankit007Kumar@users.noreply.github.com>
2024-12-28 20:58:11 +0530

    Delete Terraform directory

--- Terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "resource_group_name" {}
-variable "location" {}
-variable "vnet_name" {}
-variable "vnet_address_space" {}
-variable "subnet_name" {}
-variable "subnet_address_prefix" {}
-variable "lb_name" {}
-variable "frontend_ip_name" {}
